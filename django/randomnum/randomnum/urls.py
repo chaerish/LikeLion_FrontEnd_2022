2f6141d8f9f4719f004675946e6718c8445e5334
Bin0917 <leesd132@naver.com>
2022-05-18 01:00:15 +0900

    django 과제 re

--- django/randomnum/randomnum/urls.py
+++ django/randomnum/randomnum/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import lotto.views
-import lotto_r.views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
